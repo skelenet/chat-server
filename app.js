b95d22a2900840f2a408cd03d4f5e856d5846e1c
Ricky Chon <rickychon99@gmail.com>
2021-06-26 23:00:25 -0400

    Setup socket.io template

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server);
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) =>
 {
-    res.send('<h1>Hello World!</h1>')
+    res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(port, () =>
+io.on('connection', (socket) =>
+{
+    console.log('A user connected')
+    socket.on('disconnect', () =>
+    {
+        console.log('User disconnected')
+    })
+})
+
+server.listen(port, () =>
 {
     console.log(`Listening on *:${port}`)
 })
