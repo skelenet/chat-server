0ac0a30740ddb9d432d2c459407559323ff30646
Ricky Chon <rickychon99@gmail.com>
2021-06-27 02:10:13 -0400

    Add socket.io chatroom boilerplate  (#1)
    * Add .gitignore for node_modules
    
    * Install Express.js
    
    * Setup Express template
    
    * Use yarn instead of package-lock
    
    * Setup socket.io template
    
    * Use socket.io CDN for better performance
    
    * Remove whitespace
    
    * Emit chat message event
    
    * Broadcast chat messages to all users

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server);
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) =>
+{
+    res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) =>
+{
+    console.log('A user connected')
+    socket.on('chat message', msg =>
+    {
+        io.emit('chat message', msg)
+        console.log('message: ' + msg)
+    })
+
+    socket.on('disconnect', () =>
+    {
+        console.log('User disconnected')
+    })
+})
+
+server.listen(port, () =>
+{
+    console.log(`Listening on *:${port}`)
+})
