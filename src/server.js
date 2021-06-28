800c311c947139787a7fcc48fff18be47dc0ed7b
Ricky Chon <rickychon99@gmail.com>
2021-06-27 22:28:28 -0400

    Optimize imports for server

--- src/server.js
+++ src/server.js
@@ -1,18 +1,13 @@
 const express = require('express')
-const http = require('http')
-const { Server } = require('socket.io')
-
 const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) =>
 {
     res.sendFile(__dirname + '/index.html')
 })
-
-const server = http.createServer(app)
-const io = new Server(server)
-
-const port = process.env.PORT || 3000
 
 server.listen(port, () =>
 {
