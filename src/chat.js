9c0b64e086274743ca0ead1f7d61d42f8a9dd596
Ricky Chon <rickychon99@gmail.com>
2021-07-03 05:55:24 -0400

    Rename event namespace from `chatroom` to `chat` (#19)

--- src/chat.js
+++ src/chat.js
@@ -1,26 +1,26 @@
 module.exports = (io, socket) =>
 {
     // Notify all clients when someone leaves
-    const disconnect = () => io.emit('chatroom:leave', { id: socket.id })
+    const disconnect = () => io.emit('chat:leave', { id: socket.id })
     // Send global message to all clients except sender
-    const sendGlobalMsg = msg => socket.broadcast.emit('chatroom:global_msg_sent', { sender: { nickName: socket.nickName, id: socket.id }, msg: msg })
+    const sendGlobalMsg = msg => socket.broadcast.emit('chat:global_msg_sent', { sender: { nickName: socket.nickName, id: socket.id }, msg: msg })
 
     const setNickname = name =>
     {
         socket.nickName = name
         // Send nickname back, only to the client that set it (no one else should know)
-        io.to(socket.id).emit('chatroom:nickname_set', { nickName: name })
+        io.to(socket.id).emit('chat:nickname_set', { nickName: name })
     }
 
     const connect = () =>
     {
         // Notify all clients when someone joins
-        io.emit('chatroom:join', { id: socket.id })
+        io.emit('chat:join', { id: socket.id })
 
         socket.on('disconnect', disconnect)
-        socket.on('chatroom:send_global_msg', sendGlobalMsg)
+        socket.on('chat:send_global_msg', sendGlobalMsg)
     }
 
-    socket.on('chatroom:set_nickname', setNickname)
-    socket.on('chatroom:connect', connect)
+    socket.on('chat:set_nickname', setNickname)
+    socket.on('chat:connect', connect)
 }
